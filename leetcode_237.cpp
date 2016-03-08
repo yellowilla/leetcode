d9c07d1301d66ef5a8390fb3542784b34fc03377
yellowilla <nalu1026@gmail.com>
2016-03-08 21:55:23 +0800

    Create leetcode_237.cpp

---
+++ leetcode_237.cpp
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode* tmp;
+        tmp = node->next;
+        node->val = tmp->val;
+        node->next= tmp->next; 
+        free(tmp);
+    }
+};
