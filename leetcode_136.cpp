1c38102d01e526f6eb1a5a6a796e645e4f286c67
Yilu Wang <nalu1026@gmail.com>
2016-10-05 18:49:37 +0800

    Create leetcode_136.cpp
    Given an array of integers, every element appears twice except for one. Find that single one.

---
+++ leetcode_136.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        if(nums.size()==1)
+            return nums[0];
+        int res=nums[0];
+        for(int i=1;i<nums.size();i++)
+            res^=nums[i];
+        return res;
+    }
+};
