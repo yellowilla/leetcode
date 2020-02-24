ae5502bdcd6dd8c156b32d6341b4e56806550234
Yilu Wang <nalu1026@gmail.com>
2020-02-24 21:24:42 +0800

    Create leetcode_96. Unique Binary Search Trees.cpp
    卡特兰数

---
+++ leetcode_96. Unique Binary Search Trees.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> ans(n + 1, 0);
+        ans[0] = 1;
+        ans[1] = 1;
+        for (int i = 2; i <= n; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                    ans[i] += ans[j] * ans[i - 1 - j];
+            } 
+        }
+        
+        return ans[n];
+    }
+};
