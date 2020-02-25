45bf4a0c8da2db30f57ef3b45f382771cf7eb3dd
Yilu Wang <nalu1026@gmail.com>
2020-02-25 09:26:50 +0800

    Update leetcode_110.cpp

--- leetcode_110.cpp
+++ leetcode_110.cpp
@@ -27,3 +27,53 @@ public:
         return  l >= r ? l+1 : r+1;
     }
 };
+
+
+///////////////////////////////////////////////////////////////////////////
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        if (root == NULL)
+        {
+            return true;
+        }
+        
+        int h = 0;
+        bool flag = true;
+        getHeight(root, h, flag);
+        
+        return flag;
+    }
+    
+    void getHeight(TreeNode* root, int& h, bool& flag)
+    {
+        if (root == NULL)
+        {
+            return;
+        }
+        
+        h = 0;
+        int lh = 0, rh = 0;
+        if (root->left != NULL)
+        {
+            getHeight(root->left, lh, flag);
+        }
+        if (root->right != NULL)
+        {
+            getHeight(root->right, rh, flag);
+        }
+        
+        h = max(lh, rh) + 1;
+        flag = flag && abs(lh - rh) <= 1;
+        return;
+    }
+};
