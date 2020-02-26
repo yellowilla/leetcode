9b87c9e6190ca78a6106c535a94a5880b10c2b29
Yilu Wang <nalu1026@gmail.com>
2020-02-26 23:07:52 +0800

    Update lc_94. Binary Tree Inorder Traversal.cpp

--- lc_94. Binary Tree Inorder Traversal.cpp
+++ lc_94. Binary Tree Inorder Traversal.cpp
@@ -39,3 +39,46 @@ public:
 
 
 ///////////////////////////////////////////////////////////////////////////////////
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        if (root == NULL)
+        {
+            return res;
+        }
+        
+        deque<TreeNode*> path;
+        path.push_back(root);
+        while (!path.empty())
+        {
+            TreeNode* tmp = path.back();
+            cout << tmp->val << endl;
+            if (tmp->left != NULL)
+            {
+                path.push_back(tmp->left);
+                tmp->left = NULL;
+            }
+            else
+            {
+                res.push_back(tmp->val);
+                path.pop_back();
+                if (tmp->right != NULL)
+                {
+                    path.push_back(tmp->right);
+                }
+            }
+        }
+        
+        return res;
+    }
+};
