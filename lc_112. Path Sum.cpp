6ab7d6ec7a72f98c67eff82a2fb1a86081429553
Yilu Wang <nalu1026@gmail.com>
2020-02-28 15:42:36 +0800

    Create lc_112. Path Sum.cpp

---
+++ lc_112. Path Sum.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int sum) {
+        if (root == NULL)
+        {
+            return false;
+        }
+
+        sum = sum - root->val;
+        if (sum == 0 && root->left == NULL && root->right == NULL)
+        {
+            return true;
+        }
+        else
+        {
+            return hasPathSum(root->left, sum) || hasPathSum(root->right, sum);
+        }
+        
+    }
+};
